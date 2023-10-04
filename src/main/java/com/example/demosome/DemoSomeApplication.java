03117c18a3dfc334583d6fe8d0d594495e83312c
sinaaskarnejad <sina.askarnejad@gmail.com>
2023-10-04 04:45:13 +0330

    pulllll

--- src/main/java/com/example/demosome/DemoSomeApplication.java
+++ src/main/java/com/example/demosome/DemoSomeApplication.java
@@ -8,6 +8,7 @@ public class DemoSomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSomeApplication.class, args);
+        System.out.println("B");
     }
 
 }
--- src/test/java/com/example/demosome/DemoSomeApplicationTests.java
+++ src/test/java/com/example/demosome/DemoSomeApplicationTests.java
@@ -8,6 +8,7 @@ class DemoSomeApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("B");
     }
 
 }
