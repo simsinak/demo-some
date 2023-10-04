f949875a192dbdf49bc68682a7b9ceb24b30516c
sinaaskarnejad <sina.askarnejad@gmail.com>
2023-10-04 04:39:49 +0330

    init commit

---
+++ src/main/java/com/example/demosome/DemoSomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demosome/DemoSomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
